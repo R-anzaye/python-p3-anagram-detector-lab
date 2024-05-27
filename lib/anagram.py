344b439a58bee6bd61359fdb1c337494a051df5f
Ryan Anzaye <rileydollar8@gmail.com>
2024-05-27 21:34:48 +0300

    anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = ''.join(sorted(word.lower()))
+
+    def match(self, words):
+        matched_anagrams = []
+        for word in words:
+            sorted_word = ''.join(sorted(word.lower()))
+            if sorted_word == self.word:
+             matched_anagrams.append(word)
+        return matched_anagrams
